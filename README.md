6d7eb4aa84a104298019176bd6074fda9ad56418
Alexey Poryadin <alexey/poryadin@gmail.com>
2024-07-29 00:15:02 +0300

    Parcel + React + TS + Tailwind CSS

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + TS + Tailwind CSS
+
+This template provides a minimal setup to get React + TS + Tailwind CSS
+working in a Parcel environment. ESLint and Prettier are also set up.
+For more information on setting up the project, [see here](https://parceljs.org/docs/).
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Logo from "@/assets/react.svg";
+
+const App = () => {
+  return (
+    <main className="h-full flex flex-col items-center justify-center">
+      <h1 className="text-3xl">Hello world!</h1>
+      <Logo className="w-96" />
+    </main>
+  );
+};
+
+export default App;
